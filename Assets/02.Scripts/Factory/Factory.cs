0616a3864241845d01ca3e1856ccafc2b4ac12be
chaerin1101 <167056216+chaerin1101@users.noreply.github.com>
2024-06-21 15:11:16 +0900

    [Update] 공장 UI
    예린님이 만든 UI에 스크립트 연결해서 공장이 재화를 자동생산하게 만들기

--- Assets/02.Scripts/Factory/Factory.cs
+++ Assets/02.Scripts/Factory/Factory.cs
@@ -34,9 +34,11 @@ public class Factory : MonoBehaviour
 
     void UpdateUI()
     {
-        currentLevelText.text = "레벨 " + currentLevel;
+        currentLevelText.text = "공장 레벨 " + currentLevel;
         nextLevelText.text = "다음 레벨 " + nextLevel;
-        coinsText.text = string.Format("{0} / {1}", currentCoins, upgradeCost);
+
+        coinsText.text = $"<color=#FF0000>{currentCoins}</color> / {upgradeCost}";
+        //coinsText.text = string.Format("{0} / {1}", currentCoins, upgradeCost);
         // string.Format으로 한줄에 입력하기
 
         canUpgrade = currentCoins >= upgradeCost;   // 현재보유코인 >= 필요코인 이면 업그레이드 가능
