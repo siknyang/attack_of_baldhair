d77ffed026aeb455bf58d8e81f3535156cdfb45c
siknyang <167048578+siknyang@users.noreply.github.com>
2024-06-22 03:23:19 +0900

    [Feat] Player 정보 저장 및 불러오기

--- Assets/02.Scripts/Character/CharacterStats.cs
+++ Assets/02.Scripts/Character/CharacterStats.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public int level;
+    public UserData data;
+
+    public string nickName { get => data.nickName; set => data.nickName = value; }
+    public int level { get => data.level; set => data.level = value; }
     public float health;
-    public float attackPower;
-    public float attackSpeed;
+    public float attackPower { get => data.attackPower; set => data.attackPower = value; }
+    public float attackSpeed { get => data.attackSpeed; set => data.attackSpeed = value; }
     public float moveSpeed;
     public float attackRange;
+    public float experience { get => data.experience; set => data.experience = value; }
+    public int coin { get => data.coin; set => data.coin = value; }
+}
+
+[System.Serializable]
+public class UserData
+{
+    public string nickName;
+    public int level;
+    public float attackPower;
+    public float attackSpeed;
     public float experience;
     public int coin;
 }
