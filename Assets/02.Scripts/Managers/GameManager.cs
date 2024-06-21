702441a6141b09e132626cb59638aa66f36a19fa
siknyang <167048578+siknyang@users.noreply.github.com>
2024-06-21 20:10:23 +0900

    [Add] GameManager.cs 추가

---
+++ Assets/02.Scripts/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("GameManager").GetComponent<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    public Player player;
+
+    public Player Player
+    {
+        get { return player; }
+        set { player = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
