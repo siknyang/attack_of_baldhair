d21a32a162d7eb72333a550b0d7446df22fbebeb
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-06-25 23:19:54 +0900

    [Feat] 에너미 머리 위 피격 텍스트

--- Assets/02.Scripts/Character/HealthSystem.cs
+++ Assets/02.Scripts/Character/HealthSystem.cs
@@ -10,6 +10,7 @@ public class HealthSystem : MonoBehaviour
     public bool IsEnemy { get; set; }
 
     public bool IsDie = false;
+    [SerializeField] private GameObject hitTextPrefab;
 
     private void Start()
     {
@@ -26,12 +27,23 @@ public class HealthSystem : MonoBehaviour
         if(IsEnemy) // 에너미일 때
         {
             OnTakeDamage?.Invoke(); // 데미지 받을 때 깜빡깜빡
+            ShowHitText();
         }
 
         if (health == 0)
         {
             IsDie = true;
             OnDie?.Invoke();
+        }
+    }
+
+    private void ShowHitText()
+    {
+        if (hitTextPrefab != null)
+        {
+            // 에너미 머리 위에 텍스트 생성
+            Vector3 spawnPosition = transform.position + Vector3.up * 2;
+            GameObject hitText = Instantiate(hitTextPrefab, spawnPosition, Quaternion.identity);
         }
     }
 }
